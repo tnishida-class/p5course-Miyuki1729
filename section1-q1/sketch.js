6fe9762b6c070f649d107571188bbe5b58254000
Miyuki1729 <92025830+Miyuki1729@users.noreply.github.com>
2021-10-18 23:27:56 +0900

    Change section1-q1

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -8,11 +8,21 @@ function setup(){
   strokeWeight(25);
   strokeCap(SQUARE);
   arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
+  arc(100 - 25, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
   // BLANK[1]
+
+  /*
+  arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI)
+  arc(x, y, w, h, start, stop, [mode], [detail])
+  startとstop: 第4象限→第3象限→第2象限→第1象限の順に、時計回りでまわる。
+  0は3時、HALF_PIが6時、PIが9時、PI + HALF_PIが12時、TWO_PIが一周した3時。
+  */
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
   const red = color(196, 0, 0);
   const blue = color(14, 47, 146);
   const green = color(22, 131, 46);
   const gray = color(77);
+
+
 }
--- section2-3/sketch.js
+++ section2-3/sketch.js
@@ -5,9 +5,28 @@ function setup(){
   createCanvas(100, 100);
   for(let i = 0; i < 10; i++){
     for(let j = 0; j < 10; j++){
+      fill(i % 2 == 0 ? 255 : 0); // ここを変えます(変えた)
+      console.log(i, j);
+      rect(i * 10, j * 10, 5, 5);
+    }
+  }
+}
+
+/*
+なにか指示(条件 ? 条件が真(true)の時に実行する式 : 条件がfalseの時に実行する式)
+*/
+
+/*
+「偶数『行』の四角形の色を変える」パターン（もともとあったほう）
+function setup(){
+  createCanvas(100, 100);
+  for(let i = 0; i < 10; i++){
+    for(let j = 0; j < 10; j++){
       fill(j % 2 == 0 ? 255 : 0); // ここを変えます
       console.log(i, j);
       rect(i * 10, j * 10, 5, 5);
     }
   }
 }
+
+*/
