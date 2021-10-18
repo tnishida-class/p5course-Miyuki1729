c37107bd57707990570819b3f71c254a71a6d3ad
Miyuki1729 <92025830+Miyuki1729@users.noreply.github.com>
2021-10-19 07:09:41 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,11 @@
 // 小手調べ
 function setup(){
+  createCanvas(200, 200);
   for(let i = 0; i < 10; i++){
+    let x = (i + 1) * 10;
+    noFill();
+    stroke(i < 5 ? 'blue' : 'red');
+    ellipse(100, 100, x);
     // BLANK[1]
   }
 }
