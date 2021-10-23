51e9ba643dd8c4a5173baf65d3d14d796dff1b37
Miyuki1729 <92025830+Miyuki1729@users.noreply.github.com>
2021-10-24 00:29:00 +0900

    Change section1-q1 ver.2
    神戸市のロゴの弧の端を合うように変更しました。また、神戸大学のロゴ(赤い部分以外)を追加しました。ただ、"KOBE UNIVERSITY"のフォントや文字列の幅などを上手く再現できなかったのが心残りです。

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -1,14 +1,14 @@
 // 練習問題：神戸市のマーク
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(400, 200);
   background(255);
 
   // 空欄を埋めて神戸市のロゴマークを完成させよう
   noFill();
   strokeWeight(25);
   strokeCap(SQUARE);
-  arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  arc(100 - 25, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
+  arc(100 + 26.5, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
+  arc(100 - 26.5, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
   // BLANK[1]
 
   /*
@@ -23,6 +23,26 @@ function setup(){
   const blue = color(14, 47, 146);
   const green = color(22, 131, 46);
   const gray = color(77);
+
+  noStroke();
+  fill(blue);
+  ellipse(300, 100, 100, 100);
+  fill(255);
+  ellipse(295, 96, 93, 93);
+  fill(green);
+  ellipse(294, 96, 89, 89);
+  fill(255);
+  ellipse(292, 102, 82, 82);
+  textSize(21);
+  textFont('Serif');
+  textStyle(BOLD);
+  fill(gray);
+  text('KOBE', 260, 94);
+  textSize(9);
+  textFont('Georgia');
+  textStyle(BOLD);
+  fill(gray);
+  text('UNIVERSITY', 270, 109);
 
 
 }
