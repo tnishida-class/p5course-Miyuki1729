f13fb77129b676b09ebff98da5ac5af48aecae0a
Miyuki1729 <92025830+Miyuki1729@users.noreply.github.com>
2021-10-24 00:31:48 +0900

    Change section2-q1 ver.2

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,10 +1,12 @@
 // 小手調べ
 function setup(){
   createCanvas(200, 200);
+  const red = color(255, 0, 0);
+  const blue = color(0, 0, 255);
   for(let i = 0; i < 10; i++){
     let x = (i + 1) * 10;
     noFill();
-    stroke(i < 5 ? 'blue' : 'red');
+    stroke(i < 5 ? blue : red);
     ellipse(100, 100, x);
     // BLANK[1]
   }
