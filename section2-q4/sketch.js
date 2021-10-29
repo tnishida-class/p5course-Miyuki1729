29af6badf6950fe8e301a0115b34645727912b30
Miyuki1729 <92025830+Miyuki1729@users.noreply.github.com>
2021-10-29 18:24:15 +0900

    Change section2-q4
    イギリスの国旗を作成しました。国旗の縦横比や色を調べたり、それぞれの線の太さを工夫したりしました。また、斜めの赤い線が独特で、実物と見比べながら線の始まりと終わりの位置を決めるのが意外と難しかったです。それぞれの赤い線の傾きを同じにしたく思い、右の部分以外は傾き1/2に揃えられました。

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,21 +1,55 @@
 // ギリシャ国旗
+// function setup() {
+//   const blue = color(0, 51, 160);
+//   createCanvas(270, 180);
+//   // noStroke();
+//   background(255);
+//
+//   let d = height / 9; // 縞1本の太さ
+//
+//   for(let i = 0; i < 9; i++){
+//     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+//     rect(0, i * d, width, (i + 1) * d);
+//   }
+//
+//   fill(blue);
+//   let size = d * 5;
+//   rect(0, 0, size, size);
+//
+//   fill(255);
+//   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+// }
 function setup() {
-  const blue = color(0, 51, 160);
-  createCanvas(270, 180);
-  // noStroke();
+  const blue = color(0, 36, 125);
+  const red = color(207, 20, 43);
+  const white = color(255);
+  createCanvas(300, 150);
+  noStroke();
   background(255);
 
-  let d = height / 9; // 縞1本の太さ
-
-  for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
-  }
-
   fill(blue);
-  let size = d * 5;
-  rect(0, 0, size, size);
+  rect(0, 0, 300, 150);
 
-  fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  stroke(white);
+  strokeWeight(30);
+  line(0, 0, 300, 150);
+  line(300, 0, 0, 150);
+  stroke(red);
+  strokeWeight(10);
+  line(0, 5, 150, 80);
+  line(178, 55, 296, -4);
+  line(175, 81, 300, 145);
+  line(2, 154, 128, 91);
+
+
+
+  noStroke();
+  fill(white);
+  rect(125, 0, 50, 150);
+  rect(0, 50, 300, 50);
+  fill(red);
+  rect(135, 0, 30, 150);
+  fill(red);
+  rect(0, 60, 300, 30);
+
 }
