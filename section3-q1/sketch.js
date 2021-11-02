9d8da7e84a74babdc9a638fafe3a01ee806f7c88
Miyuki1729 <92025830+Miyuki1729@users.noreply.github.com>
2021-11-02 09:35:06 +0900

    Change section3-q1 ver.2
    より心臓らしい動きになるように工夫しました。心電図の画像を見て作ったり、cycleの値を工夫したりしました。左矢印キーを押すと、押していないときよりも鼓動が速くなります。

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,11 +1,13 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let x;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
-  cycle = 68;
+  cycle = 97;
+  x = 0;
 }
 
 function draw(){
@@ -18,17 +20,58 @@ function draw(){
   }
 
   if(count < 6){
-    ellipse(width / 2, height / 2, 7 + 3 * count);
+    x = 21 + count;
+    // ellipse(width / 2, height / 2, 21 + count);
   }
   else if(count < 12){
-    ellipse(width / 2, height / 2, 7 + 3 * (12 - count));
+    x = 21 + 12 - count;
+    // ellipse(width / 2, height / 2, 21 + 12 - count);
     }
   else if(count < 24){
-    ellipse(width / 2, height / 2, 7 + 7 * 2 * (count - 12));
+    x = 21 + 7 * (count - 12);
+          // ellipse(width / 2, height / 2, 21 + 7 * 2 * (count - 12));
     }
   else if(count < 32){
-    ellipse(width / 2, height / 2, 175 + 7 * 3 * (24 - count));
+    x = 105 - 7 * (count - 24) * 15 / 8;
+  // else if(count < 24){
+  //   x = 21 + 7 * 2 * (count - 12);
+  //   // ellipse(width / 2, height / 2, 21 + 7 * 2 * (count - 12));
+  //   }
+  // else if(count < 32){
+  //   x = 189 + 7 * 3 * (24 - count - (count - 24) / 8);
+    // ellipse(width / 2, height / 2, 189 + 7 * 3 * (24 - count - (count - 24) / 8));
   }
+  else if(count < 48){
+    x = 7 * ((count - 32) / 2);
+  }
+  else if(count < 64){
+    x = 21 + 7 * ((64 - count) * 5 / 16);
+  }
+  // else if(count < 41){
+  //   x = (count - 32) * 8 / 3;
+  //   // ellipse(width / 2, height / 2, (count - 32) * 2.7);
+  // }
+  // else if(count < 46){
+  //   x = 24 - (count - 41) * 3 / 5;
+  //   // ellipse(width / 2, height / 2, ())
+  // }
+  // else if(count < 54){
+  //   x = 21 + (count - 46) * 7;
+  // }
+  // else if(count < 64){
+  //   x = 77 - (count - 54 - (count - 54) / 5) * 7;
+  // }
+  // else if(count < 68){
+  //   x = 21 + count - 64;
+  // }
+  // else if(count < 72){
+  //   x = 21 + 72 - count;
+  // }
+  else{
+    x = 21;
+    // ellipse(width / 2, height / 2, 21);
+  }
+  ellipse(width / 2, height / 2, x);
   // else if(count < 42){
   //   ellipse(width / 2, height / 2, 77 - 6 * (count - 42));
   // }
@@ -38,7 +81,9 @@ function draw(){
   // else if(count < 56){
   //   ellipse(width / 2, height / 2, 7 + 8 * (count - 46));
   // }
-
+  // 64 - 54 - x = 8     54 - 54 - x = 0
+  // x = 2                x = 0
+  // (x - 54) / 5
 }
 
 
